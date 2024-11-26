c955c13a4e97b9bcd7590fcb75920bf8cace7d1b
adis-tek <67308354+adis-tek@users.noreply.github.com>
2024-11-25 18:05:57 -0600

    conversion to yarn monorepo successful + operational

---
+++ yarn-monorepo/packages/site-info/index.js
@@ -0,0 +1,8 @@
+const PROJECT = {
+  title: "Next 14.0 with Payload 3.0",
+  subtitle: "A monorepo setup with Next.js 14.0 and Payload 3.0",
+};
+
+export function getSiteInfo() {
+  return { title: PROJECT.title, subtitle: PROJECT.subtitle };
+}
